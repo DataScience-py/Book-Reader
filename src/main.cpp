d8f15a917f2696452d467d418d63fd5508c09bb5
Anton Golubchikov <anton1programmist@gmail.com>
2025-08-01 16:13:45 +0700

    Add small application

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <QApplication>
+#include <QLabel>
+
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+    QLabel label("Привет, Qt!");
+    label.show();
+    return app.exec();
+}
